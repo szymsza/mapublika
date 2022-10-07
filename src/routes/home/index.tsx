6a2fa7f6af71253d8fbdb70798754ca36604aa52
Jakub Szymsza <jakub@szymsza.cz>
2022-10-07 23:32:04 +0200

    Homepage maps

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -1,8 +1,15 @@
 import { h } from 'preact';
 
+import Regions from '../../assets/maps/regions';
+import Counties from '../../assets/maps/counties';
+import Towns from '../../assets/maps/towns';
+
 const Home = () => (
   <div className="home">
     <h1>Home</h1>
+    <Regions />
+    <Counties />
+    <Towns />
     <p>This is the Home component.</p>
   </div>
 );
